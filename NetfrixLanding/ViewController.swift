8db3a2a8aa8974b673a31338a9ac9fe4dbbf2bb8
jacobkosmart <jacobkosmart@gmail.com>
2023-02-03 14:31:47 +0900

    Initial Commit

---
+++ NetfrixLanding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetfrixLanding
+//
+//  Created by Jacob Ko on 2023/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
